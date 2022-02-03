19c7f942a5001dd2d7f8ef035b3bb79231fc84ab
SelimHorri <horri.slim@gmail.com>
2022-02-03 14:37:32 +0100

    add remain creations and inserts of all tables

---
+++ src/main/resources/db/migration/dev/V30__insert_service_details_table.sql
@@ -0,0 +1,24 @@
+
+INSERT INTO service_details (name, description, is_available, duration, price_unit, category_id) VALUES
+('coupe courte', 'a quick and tiny haircut;', true, 15, 10.0, 1),
+('coupe tres courte', 'coupe avec tendeuse w makinet;;', true, 15, 15.0, 1),
+('shampoing + sechage cheveux courts', 'm3aha talya shampoing w m3aha chaye7 rasek;', true, 10, 5.0, 2),
+('shampoing + brushing cheveux courts', NULL, true, 30, 30.0, 3),
+('shampoing + brushing cheveux longs', NULL, true, 30, 30.5, 3),
+('coupe courte', 'a quick and tiny haircut;', true, 15, 10.0, 4),
+('coupe tres courte', 'coupe avec tendeuse w makinet;;', true, 15, 15.0, 4),
+('shampoing + sechage cheveux courts', 'm3aha talya shampoing w m3aha chaye7 rasek;', true, 10, 5.0, 5),
+('shampoing + sechage cheveux longs', 'm3aha talya shampoing w m3aha chaye7 rasek;', true, 10, 5.5, 5),
+('shampoing + brushing cheveux courts', NULL, true, 30, 30.0, 6),
+('shampoing + brushing cheveux longs', NULL, true, 30, 30.5, 6),
+('shampoing + brushing cheveux tres longs', NULL, true, 30, 40, 6),
+('coupe courte', 'a quick and tiny haircut;', true, 15, 10.0, 7),
+('coupe tres courte', 'coupe avec tendeuse w makinet;;', true, 15, 15.0, 7),
+('shampoing + sechage cheveux courts', 'm3aha talya shampoing w m3aha chaye7 rasek;', true, 10, 5.0, 8),
+('shampoing + brushing cheveux courts', NULL, true, 30, 30.0, 9);
+
+
+
+
+
+
