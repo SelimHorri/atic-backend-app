266a5aeb393292b1e0645ab372580762a93cc30d
SelimHorri <horri.slim@gmail.com>
2022-02-09 19:25:18 +0100

    rectify sql files for test and prod

--- src/main/resources/db/migration/prod/V11__create_ratings_table.sql
+++ src/main/resources/db/migration/prod/V11__create_ratings_table.sql
@@ -3,8 +3,8 @@ CREATE TABLE ratings (
 	employee_id INT NOT NULL,
 	customer_id INT NOT NULL,
 	rate_date TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP(),
-	rate DECIMAL(2, 1) NOT NULL,
-	rate_description VARCHAR(255),
+	rate VARCHAR(255) NOT NULL,
+	description VARCHAR(255),
 	created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP(),
 	updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP() ON UPDATE CURRENT_TIMESTAMP(),
 	PRIMARY KEY (employee_id, customer_id, rate_date)
