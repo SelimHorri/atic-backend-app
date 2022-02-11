1ad89d1dc5b2fdc247af1d46be41733cc949210e
SelimHorri <horri.slim@gmail.com>
2022-02-11 21:24:12 +0100

    init dao layer

---
+++ src/main/java/tn/cita/app/repository/CredentialRepository.java
@@ -0,0 +1,20 @@
+package tn.cita.app.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import tn.cita.app.domain.entity.Credential;
+
+public interface CredentialRepository extends JpaRepository<Credential, Integer> {
+	
+	
+	
+}
+
+
+
+
+
+
+
+
+
---
+++ src/main/java/tn/cita/app/repository/ServiceDetailRepository.java
@@ -0,0 +1,21 @@
+package tn.cita.app.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import tn.cita.app.domain.entity.ServiceDetail;
+
+public interface ServiceDetailRepository extends JpaRepository<ServiceDetail, Integer> {
+	
+	
+	
+}
+
+
+
+
+
+
+
+
+
+
