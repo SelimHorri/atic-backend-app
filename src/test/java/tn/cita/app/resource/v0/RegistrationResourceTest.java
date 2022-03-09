606e3f8bbbad93eb4b1394d12d7d4aabc1218f55
SelimHorri <horri.slim@gmail.com>
2022-03-09 23:11:50 +0100

    create unit and integration tests for different classes

--- src/test/java/tn/cita/app/resource/v0/RegistrationResourceTest.java
+++ src/test/java/tn/cita/app/resource/v0/RegistrationResourceTest.java
@@ -1,12 +1,23 @@
 package tn.cita.app.resource.v0;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.reactive.server.WebTestClient;
+
+import tn.cita.app.service.RegistrationService;
 
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 @AutoConfigureWebTestClient
 class RegistrationResourceTest {
+	
+	@Autowired
+	private WebTestClient webTestClient;
+	
+	@MockBean
+	private RegistrationService registrationService;
 	
 	
 	
