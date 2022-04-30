557b84676a3b8dcba597d224ff2909af5d31fc4c
SelimHorri <horri.slim@gmail.com>
2022-04-30 18:35:11 +0100

    terminate process of db setup

--- src/main/resources/db/migration/staging/V26__insert_reservations_table.sql
+++ src/main/resources/db/migration/staging/V26__insert_reservations_table.sql
@@ -7,6 +7,20 @@ INSERT INTO reservations (code, description, status, customer_id) VALUES
 ('566996bf-179f-4db5-960e-e0351babd194', 'done', 'COMPLETED', 5),
 ('d0f37d34-8608-4127-b704-2d2a21386f18', NULL, 'NOT_STARTED', 3),
 ('81bc3955-a57b-4c62-96cd-2d2e0194bc99', 'ASAP', 'IN_PROGRESS', 4),
-('29d51409-720b-4e38-a9f1-5eb8e81dd079', NULL, 'COMPLETED', 8);
+('29d51409-720b-4e38-a9f1-5eb8e81dd079', NULL, 'COMPLETED', 8),
+('64c91a2e-804d-4f76-8374-0b61095860d0', 'i need to do this!', 'NOT_STARTED', 1),
+('2f58a400-ca68-49ab-a005-6ddcce163796', 'alot of quick!', 'NOT_STARTED', 1),
+('39c2fb12-4a6d-4c1f-b8c8-6f049e169501', NULL, 'COMPLETED', 3),
+('39c2fb12-4a6d-4c1f-b8c8-6f049e169501', NULL, 'COMPLETED', 3),
+('a09c32d8-cece-44cd-b30a-8f0b20a050b6', NULL, 'COMPLETED', 4),
+('e65e223e-c7cb-4590-b9a4-5fe9ed934c93', NULL, 'IN_PROGRESS', 5),
+('4126efd8-c456-45ae-b4ce-be589d2ffde6', NULL, 'IN_PROGRESS', 5),
+('90c9b98b-347e-4380-a517-f504a70124d0', NULL, 'NOT_STARTED', 5),
+('cfa8dea1-3126-4e23-8c8a-850857d06cd4', NULL, 'NOT_STARTED', 6),
+('759ed6e3-44fd-48fa-b8dc-06b181de3175', NULL, 'IN_PROGRESS', 6),
+('b465317c-43df-41bb-9127-d0d75f8e1ad5', NULL, 'IN_PROGRESS', 7),
+('d41728ab-f7d2-432f-b6a5-7486b89decee', NULL, 'NOT_STARTED', 7),
+('9bfec1a9-42a3-4985-8905-61df4c8c6321', NULL, 'IN_PROGRESS', 8),
+('0c6da640-79fd-451a-8e87-3614b57e8fef', NULL, 'NOT_STARTED', 8);
 
 
