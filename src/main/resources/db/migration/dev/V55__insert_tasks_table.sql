7a83c9d7f52cb8014de7d0c739a29b40ef999800
SelimHorri <horri.slim@gmail.com>
2022-04-30 17:53:47 +0100

    create tasks table and inserts in dev migration

---
+++ src/main/resources/db/migration/dev/V55__insert_tasks_table.sql
@@ -0,0 +1,22 @@
+
+INSERT INTO tasks (worker_id, reservation_id, start_date, end_date, worker_description, manager_description) VALUES
+(1, 1, NULL, NULL, NULL, 'We need endup with this very quickly'),
+(1, 2, CURRENT_TIMESTAMP(), CURRENT_TIMESTAMP(), 'finished success', 'great'),
+(1, 3, CURRENT_TIMESTAMP(), NULL, 'On my way to finish', 'waiting u to complete'),
+(1, 4, CURRENT_TIMESTAMP(), NULL, 'On my way to finish erger', 'waiting u to completeerger'),
+(2, 5, CURRENT_TIMESTAMP(), CURRENT_TIMESTAMP(), 'finished successrft', 'great!'),
+(2, 6, NULL, NULL, NULL, 'starts very soon!'),
+(2, 7, CURRENT_TIMESTAMP(), NULL, NULL, 'waiting u to completedf'),
+(5, 8, CURRENT_TIMESTAMP(), CURRENT_TIMESTAMP(), 'finished success fgh', 'great!!'),
+(5, 9, NULL, NULL, NULL, NULL),
+(5, 10, NULL, NULL, NULL, 'start asap plz!'),
+(5, 11, CURRENT_TIMESTAMP(), CURRENT_TIMESTAMP(), 'finished success!!', 'great!!'),
+(6, 12, CURRENT_TIMESTAMP(), CURRENT_TIMESTAMP(), 'finished successdfg', 'greatdf'),
+(6, 13, CURRENT_TIMESTAMP(), CURRENT_TIMESTAMP(), 'finished successdt', 'great drg'),
+(6, 14, CURRENT_TIMESTAMP(), NULL, 'On my way to finish', NULL),
+(7, 15, CURRENT_TIMESTAMP(), NULL, 'On my way to finish', 'waiting u to complete'),
+(8, 16, NULL, NULL, NULL, 'we need awesome work on this'),
+(8, 17, NULL, NULL, NULL, NULL);
+
+
+
