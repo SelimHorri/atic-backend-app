632961a21c44b4e5bf2140efcdcac0282c9fc769
SelimHorri <horri.slim@gmail.com>
2022-06-13 23:07:39 +0100

    add reservation detail

--- src/main/java/tn/cita/app/resource/v0/business/employee/worker/WorkerReservationResource.java
+++ src/main/java/tn/cita/app/resource/v0/business/employee/worker/WorkerReservationResource.java
@@ -7,6 +7,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,6 +17,7 @@ import lombok.RequiredArgsConstructor;
 import tn.cita.app.constant.AppConstant;
 import tn.cita.app.dto.TaskDto;
 import tn.cita.app.dto.request.ClientPageRequest;
+import tn.cita.app.dto.response.ReservationDetailResponse;
 import tn.cita.app.dto.response.api.ApiPayloadResponse;
 import tn.cita.app.service.v0.business.employee.worker.WorkerReservationService;
 import tn.cita.app.util.UserRequestExtractorUtil;
@@ -42,6 +44,14 @@ public class WorkerReservationResource {
 		final var reservations = this.workerReservationService.getAllReservations(this.userRequestExtractorUtil
 				.extractUsername(webRequest));
 		return ResponseEntity.ok(new ApiPayloadResponse<>(reservations.getSize(), HttpStatus.OK, true, reservations));
+	}
+	
+	@GetMapping("/details/{reservationId}")
+	public ResponseEntity<ApiPayloadResponse<ReservationDetailResponse>> getReservationDetails(final WebRequest request,
+			@PathVariable final String reservationId) {
+		this.userRequestExtractorUtil.extractUsername(request);
+		return ResponseEntity.ok(new ApiPayloadResponse<>(1, HttpStatus.OK, true, 
+				this.workerReservationService.getReservationDetails(Integer.parseInt(reservationId))));
 	}
 	
 	
--- src/main/java/tn/cita/app/service/v0/business/employee/worker/impl/WorkerReservationServiceImpl.java
+++ src/main/java/tn/cita/app/service/v0/business/employee/worker/impl/WorkerReservationServiceImpl.java
@@ -8,7 +8,10 @@ import org.springframework.transaction.annotation.Transactional;
 import lombok.RequiredArgsConstructor;
 import tn.cita.app.dto.TaskDto;
 import tn.cita.app.dto.request.ClientPageRequest;
+import tn.cita.app.dto.response.ReservationDetailResponse;
 import tn.cita.app.service.v0.EmployeeService;
+import tn.cita.app.service.v0.OrderedDetailService;
+import tn.cita.app.service.v0.ReservationService;
 import tn.cita.app.service.v0.TaskService;
 import tn.cita.app.service.v0.business.employee.worker.WorkerReservationService;
 
@@ -19,6 +22,8 @@ public class WorkerReservationServiceImpl implements WorkerReservationService {
 	
 	private final EmployeeService employeeService;
 	private final TaskService taskService;
+	private final ReservationService reservationService;
+	private final OrderedDetailService orderedDetailService;
 	
 	@Override
 	public Page<TaskDto> getAllReservations(final String username, final ClientPageRequest clientPageRequest) {
@@ -28,6 +33,18 @@ public class WorkerReservationServiceImpl implements WorkerReservationService {
 	@Override
 	public Page<TaskDto> getAllReservations(final String username) {
 		return new PageImpl<>(this.taskService.findAllByWorkerId(this.employeeService.findByUsername(username).getId()));
+	}
+	
+	@Override
+	public ReservationDetailResponse getReservationDetails(final Integer reservationId) {
+		
+		final var reservationDto = this.reservationService.findById(reservationId);
+		
+		return ReservationDetailResponse.builder()
+				.reservationDto(reservationDto)
+				.orderedDetailDtos(this.orderedDetailService.findAllByReservationId(reservationDto.getId()))
+				.taskDtos(this.taskService.findAllByReservationId(reservationDto.getId()))
+				.build();
 	}
 	
 	
