f0dceda4c8234385ba36922e48944a480f95ab35
SelimHorri <horri.slim@gmail.com>
2022-07-02 19:52:53 +0100

    create classes

---
+++ src/main/java/tn/cita/app/resource/v0/business/employee/manager/ManagerCategoryResource.java
@@ -0,0 +1,29 @@
+package tn.cita.app.resource.v0.business.employee.manager;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.RequiredArgsConstructor;
+import tn.cita.app.constant.AppConstant;
+
+@RestController
+@RequestMapping(AppConstant.API_CONTEXT_V0 + "/employees/managers/categories")
+@RequiredArgsConstructor
+public class ManagerCategoryResource {
+	
+	
+	
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
