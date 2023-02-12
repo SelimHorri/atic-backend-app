c1f9c69b6d83e8af660ee07566e21dffa589cee0
SelimHorri <horri.slim@gmail.com>
2023-02-12 20:25:34 +0100

    refactor configprop

--- src/main/java/tn/cita/app/config/props/JwtConfigProps.java
+++ src/main/java/tn/cita/app/config/props/JwtConfigProps.java
@@ -1,29 +1,9 @@
 package tn.cita.app.config.props;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.boot.context.properties.ConstructorBinding;
-
-import lombok.Data;
 
 @ConfigurationProperties(prefix = "app.security.jwt")
-@ConstructorBinding
-@Data
-public final class JwtConfigProps {
-	
-	/**
-	 * Period that jwt token expires after.
-	 * in millis
-	 */
-	private final int tokenExpiresAfter;
-	
-	/**
-	 * Jwt secret key.
-	 */
-	private final String secretKey;
-	
-}
-
-
+public record JwtConfigProps(long tokenExpiresAfter, String secretKey) {}
 
 
 
